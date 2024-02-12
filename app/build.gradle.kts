0b69c9413e4eefc260e55336aaca7f7a9e6fef27
gamingPanther3 <max.lemberg2402@gmail.com>
2024-02-12 13:34:00 +0100

    changed version from 1.6.0 to 1.6.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         minSdk = 29
         targetSdk = 33
         versionCode = 1
-        versionName = "1.6.0"
+        versionName = "1.6.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
