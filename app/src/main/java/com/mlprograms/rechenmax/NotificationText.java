12a8cce6bb4cf8b600d49586fabad7b129b46201
gamingPanther3 <max.lemberg2402@gmail.com>
2024-02-15 10:57:09 +0100

    renamed NotificationHints.java to NotificationText.java

---
+++ app/src/main/java/com/mlprograms/rechenmax/NotificationText.java
@@ -0,0 +1,93 @@
+package com.mlprograms.rechenmax;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class NotificationText {
+    public static List<String> notificationHintsListGerman = Arrays.asList(
+            "Wusstest du, dass der RechenMax nur von einer einzigen Person erschaffen wurde?",
+            "Wusstest du, dass du fast alles in den Einstellungen anpassen kannst?",
+            "Wusstest du, dass du durch Klicken auf das Fragezeichen in den Einstellungen eine Anleitung erhältst?",
+            "Wusstest du, dass der Name der App vom Onkel des Erfinders stammt?",
+            "Wusstest du, dass die Android-Version vom RechenMax etwa am 01.10.23 zum Leben erweckt wurde?",
+            "Wusstest du, dass es auch eine Windows-Version von RechenMax gibt, die während des Praktikums des Erfinders erstellt wurde?",
+            "Wusstest du, dass du durch Drücken des Knopfes oben links mehr Funktionen nutzen kannst?",
+            "Wusstest du, dass der Vereinfachte Berechnungsmodus, den du in den Einstellungen anpassen kannst, vieles vereinfachen kann?",
+            "Wusstest du, dass der RechenMax am 01.10.2023 Geburtstag hat?",
+            "Wusstest du, dass der Taschenrechner ursprünglich nur für Windows gedacht war?",
+            "Wusstest du, dass der Taschenrechner in Deutschland entwickelt wurde?",
+            "Wusstest du, dass ",
+            "Wusstest du, dass ",
+            "Wusstest du, dass ",
+            "Wusstest du, dass ",
+            "Wusstest du, dass ",
+            "Wusstest du, dass ",
+            "Wusstest du, dass ",
+            "Wusstest du, dass ",
+            "Wusstest du, dass ",
+            "Wusstest du, dass ",
+            "Wusstest du, dass ",
+            "Wusstest du, dass ",
+            "Wusstest du, dass ",
+            "Wusstest du, dass ",
+            "Wusstest du, dass "
+    );
+
+    public static List<String> notificationHintsListEnglish = Arrays.asList(
+            "Did you know that the RechenMax was created by just one person?",
+            "Did you know that you can customize almost everything in the settings?",
+            "Did you know that clicking the question mark in Settings will give you instructions?",
+            "Did you know that the name of the app comes from the inventor's uncle?",
+            "Did you know that the Android version of RechenMax came to life around October 1st, 2023?",
+            "Did you know that there is also a Windows version of RechenMax, which was created during the inventor's internship?",
+            "Did you know that you can use more functions by pressing the button on the top left?",
+            "Did you know that the Simplified Calculation Mode, which you can customize in the settings, can make a lot of things easier?",
+            "Did you know that RechenMax's birthday is on October 1st, 2023?",
+            "Did you know that the calculator was originally intended only for Windows?",
+            "Did you know that the calculator was developed in Germany?",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            ""
+    );
+
+    public static List<String> notificationHintsListFrench = Arrays.asList(
+            "Savais-tu que RechenMax n'a été créé que par une seule personne?",
+            "Savais-tu que tu peux personnaliser presque tout dans les paramètres?",
+            "Savais-tu que tu peux obtenir un guide en cliquant sur le point d'interrogation dans les paramètres?",
+            "Savais-tu que le nom de l'application vient de l'oncle de l'inventeur?",
+            "Savais-tu que la version Android de RechenMax a été lancée vers le 01.10.23?",
+            "Savais-tu qu'il existe également une version Windows de RechenMax créée pendant le stage de l'inventeur?",
+            "Savais-tu que tu peux utiliser plus de fonctionnalités en appuyant sur le bouton en haut à gauche?",
+            "Savais-tu que le mode de calcul simplifié, que tu peux ajuster dans les paramètres, peut simplifier beaucoup de choses?",
+            "Savais-tu que l'anniversaire de RechenMax est le 01.10.2023?",
+            "Savais-tu que la calculatrice était à l'origine destinée uniquement à Windows?",
+            "Savais-tu que la calculatrice a été développée en Allemagne?",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            ""
+    );
+ }
