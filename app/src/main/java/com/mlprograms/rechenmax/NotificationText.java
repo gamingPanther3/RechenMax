b79f1c2902c48ad7296930818c7919dcb697f1a5
gamingPanther3 <max.lemberg2402@gmail.com>
2024-02-15 21:45:07 +0100

    added languages and translated text

--- app/src/main/java/com/mlprograms/rechenmax/NotificationText.java
+++ app/src/main/java/com/mlprograms/rechenmax/NotificationText.java
@@ -4,90 +4,175 @@ import java.util.Arrays;
 import java.util.List;
 
 public class NotificationText {
+    public static List<String> mainNotificationTitleGerman = Arrays.asList(
+            "Vergiss mich nicht!",
+            "Es wird Zeit zu rechnen!",
+            "Rechenzeit!",
+            "RechenMax wartet!"
+    );
+
+    public static List<String> mainNotificationContentGerman = Arrays.asList(
+            "Hey, du hast schon eine Weile nichts mehr gerechnet. Vielleicht wird es mal wieder Zeit.",
+            "Dein RechenMax gähnt vor Langeweile. Zeit für ein paar knifflige Berechnungen!",
+            "Berechne die Antwort auf das Leben, das Universum und alles!",
+            "RechenMax freut sich auf dich!"
+    );
+
     public static List<String> notificationHintsListGerman = Arrays.asList(
-            "Wusstest du, dass der RechenMax nur von einer einzigen Person erschaffen wurde?",
-            "Wusstest du, dass du fast alles in den Einstellungen anpassen kannst?",
-            "Wusstest du, dass du durch Klicken auf das Fragezeichen in den Einstellungen eine Anleitung erhältst?",
-            "Wusstest du, dass der Name der App vom Onkel des Erfinders stammt?",
-            "Wusstest du, dass die Android-Version vom RechenMax etwa am 01.10.23 zum Leben erweckt wurde?",
-            "Wusstest du, dass es auch eine Windows-Version von RechenMax gibt, die während des Praktikums des Erfinders erstellt wurde?",
-            "Wusstest du, dass du durch Drücken des Knopfes oben links mehr Funktionen nutzen kannst?",
-            "Wusstest du, dass der Vereinfachte Berechnungsmodus, den du in den Einstellungen anpassen kannst, vieles vereinfachen kann?",
-            "Wusstest du, dass der RechenMax am 01.10.2023 Geburtstag hat?",
-            "Wusstest du, dass der Taschenrechner ursprünglich nur für Windows gedacht war?",
-            "Wusstest du, dass der Taschenrechner in Deutschland entwickelt wurde?",
-            "Wusstest du, dass ",
-            "Wusstest du, dass ",
-            "Wusstest du, dass ",
-            "Wusstest du, dass ",
-            "Wusstest du, dass ",
-            "Wusstest du, dass ",
-            "Wusstest du, dass ",
-            "Wusstest du, dass ",
-            "Wusstest du, dass ",
-            "Wusstest du, dass ",
-            "Wusstest du, dass ",
-            "Wusstest du, dass ",
-            "Wusstest du, dass ",
-            "Wusstest du, dass ",
-            "Wusstest du, dass "
+            "Der RechenMax wurde nur von einer einzigen Person erschaffen.",
+            "Du kannst fast alles in den Einstellungen anpassen.",
+            "Du erhältst eine Anleitung, indem du auf das Fragezeichen in den Einstellungen klickst.",
+            "Der Name der App stammt vom Onkel des Erfinders.",
+            "Die Android-Version des RechenMax wurde etwa am 01.10.23 zum Leben erweckt.",
+            "Es gibt auch eine Windows-Version vom RechenMax.",
+            "Du kannst mehr Funktionen nutzen, indem du oben links auf den Knopf drückst.",
+            "Der vereinfachte Berechnungsmodus in den Einstellungen kann vieles erleichtern.",
+            "Der RechenMax hat am 01.10.2023 Geburtstag.",
+            "Der Taschenrechner war ursprünglich nur für Windows gedacht.",
+            "Der Taschenrechner wurde in Deutschland entwickelt.",
+            "42 ist die Antwort auf das gesamte Universum.",
+            "Im RechenMax gibt es neben den Grundrechenarten auch weitere Funktionen.",
+            "Der Taschenrechner wurde mithilfe von Java erstellt",
+            "Der RechenMax wird regelmäßig aktualisiert.",
+            "Es wird ständig am Taschenrechner weiter gearbeitet.",
+            "Hoffentlich gefällt dir der RechenMax.",
+            "Der RechenMax war das erste bedeutende Projekt des Entwicklers.",
+            "Deine Berechnungen sind sicher gespeichert.",
+            "Du kannst die Farbe des Taschenrechners in den Einstellungen anpassen.",
+            "Der RechenMax kann mehr als nur einfache Mathematik!",
+            "Im Hilfebereich in den Einstellungen findest du nützliche Hinweise und Tipps.",
+            "Den RechenMax und weitere Projekte findest du auf GitHub.",
+            "Besuche doch: 'https://sites.google.com/view/ml-programs' um mehr zu erfahren.",
+            "1 + 1 ergibt 2.",
+            "Besuche doch: 'https://github.com/gamingPanther3' um mehr zu erfahren."
+    );
+
+    public static List<String> mainNotificationTitleEnglish = Arrays.asList(
+            "Don't forget me!",
+            "It's time to calculate!",
+            "Calculating time!",
+            "RechenMax is waiting!"
+    );
+
+    public static List<String> mainNotificationContentEnglish = Arrays.asList(
+            "Hey, you haven't calculated anything in a while. Maybe it's time to do it again.",
+            "Your RechenMax is yawning out of boredom. Time for some tricky calculations!",
+            "Calculate the answer to life, the universe, and everything!",
+            "RechenMax is looking forward to seeing you!"
     );
 
     public static List<String> notificationHintsListEnglish = Arrays.asList(
-            "Did you know that the RechenMax was created by just one person?",
-            "Did you know that you can customize almost everything in the settings?",
-            "Did you know that clicking the question mark in Settings will give you instructions?",
-            "Did you know that the name of the app comes from the inventor's uncle?",
-            "Did you know that the Android version of RechenMax came to life around October 1st, 2023?",
-            "Did you know that there is also a Windows version of RechenMax, which was created during the inventor's internship?",
-            "Did you know that you can use more functions by pressing the button on the top left?",
-            "Did you know that the Simplified Calculation Mode, which you can customize in the settings, can make a lot of things easier?",
-            "Did you know that RechenMax's birthday is on October 1st, 2023?",
-            "Did you know that the calculator was originally intended only for Windows?",
-            "Did you know that the calculator was developed in Germany?",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            ""
+            "RechenMax was created by only one person.",
+            "You can customize almost everything in the settings.",
+            "You'll receive a guide by clicking on the question mark in the settings.",
+            "The app's name comes from the inventor's uncle.",
+            "The Android version of RechenMax came to life around 01.10.23.",
+            "There's also a Windows version of RechenMax.",
+            "You can access more functions by pressing the button in the top left corner.",
+            "The simplified calculation mode in the settings can make things easier.",
+            "RechenMax's birthday is on 01.10.2023.",
+            "The calculator was originally intended for Windows only.",
+            "The calculator was developed in Germany.",
+            "42 is the answer to the entire universe.",
+            "In RechenMax, there are additional functions besides the basic arithmetic operations.",
+            "The calculator was created using Java.",
+            "RechenMax is regularly updated.",
+            "Work on the calculator is constantly ongoing.",
+            "Hope you like RechenMax.",
+            "RechenMax was the developer's first significant project.",
+            "Your calculations are securely saved.",
+            "You can customize the color of the calculator in the settings.",
+            "RechenMax can do more than just simple mathematics!",
+            "In the help section in the settings, you'll find useful hints and tips.",
+            "You can find RechenMax and other projects on GitHub.",
+            "Visit: 'https://sites.google.com/view/ml-programs' to learn more.",
+            "1 + 1 equals 2.",
+            "Visit: 'https://github.com/gamingPanther3' to learn more."
+    );
+
+
+    public static List<String> mainNotificationTitleFrench = Arrays.asList(
+            "N'oublie pas de moi!",
+            "Il est temps de calculer!",
+            "Temps de calcul!",
+            "RechenMax attend!"
+    );
+
+    public static List<String> mainNotificationContentFrench = Arrays.asList(
+            "Hé, tu n'as pas calculé depuis un moment. Peut-être est-il temps de s'y remettre.",
+            "Ton RechenMax s'ennuie à mourir. Il est temps de se lancer dans des calculs complexes!",
+            "Calcule la réponse à la vie, à l'univers et à tout!",
+            "RechenMax a hâte de te voir!"
     );
 
     public static List<String> notificationHintsListFrench = Arrays.asList(
-            "Savais-tu que RechenMax n'a été créé que par une seule personne?",
-            "Savais-tu que tu peux personnaliser presque tout dans les paramètres?",
-            "Savais-tu que tu peux obtenir un guide en cliquant sur le point d'interrogation dans les paramètres?",
-            "Savais-tu que le nom de l'application vient de l'oncle de l'inventeur?",
-            "Savais-tu que la version Android de RechenMax a été lancée vers le 01.10.23?",
-            "Savais-tu qu'il existe également une version Windows de RechenMax créée pendant le stage de l'inventeur?",
-            "Savais-tu que tu peux utiliser plus de fonctionnalités en appuyant sur le bouton en haut à gauche?",
-            "Savais-tu que le mode de calcul simplifié, que tu peux ajuster dans les paramètres, peut simplifier beaucoup de choses?",
-            "Savais-tu que l'anniversaire de RechenMax est le 01.10.2023?",
-            "Savais-tu que la calculatrice était à l'origine destinée uniquement à Windows?",
-            "Savais-tu que la calculatrice a été développée en Allemagne?",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            ""
+            "RechenMax a été créé par une seule personne.",
+            "Vous pouvez personnaliser presque tout dans les paramètres.",
+            "Vous recevrez un guide en cliquant sur le point d'interrogation dans les paramètres.",
+            "Le nom de l'application vient de l'oncle de l'inventeur.",
+            "La version Android de RechenMax est apparue vers le 01.10.23.",
+            "Il existe également une version Windows de RechenMax.",
+            "Le mode de calcul simplifié dans les paramètres peut faciliter les choses.",
+            "L'anniversaire de RechenMax est le 01.10.2023.",
+            "La calculatrice était initialement destinée à Windows uniquement.",
+            "La calculatrice a été développée en Allemagne.",
+            "42 est la réponse à l'univers entier.",
+            "La calculatrice a été créée en utilisant Java.",
+            "RechenMax est régulièrement mis à jour.",
+            "Le travail sur la calculatrice est en cours de développement constant.",
+            "J'espère que vous aimerez RechenMax.",
+            "RechenMax était le premier projet significatif du développeur.",
+            "Vos calculs sont enregistrés en toute sécurité.",
+            "Vous pouvez personnaliser la couleur de la calculatrice dans les paramètres.",
+            "RechenMax peut faire plus que de simples mathématiques !",
+            "Dans la section d'aide dans les paramètres, vous trouverez des conseils utiles.",
+            "Vous pouvez trouver RechenMax et d'autres projets sur GitHub.",
+            "Visitez : 'https://sites.google.com/view/ml-programs' pour en savoir plus.",
+            "1 + 1 égale 2.",
+            "Visitez : 'https://github.com/gamingPanther3' pour en savoir plus."
     );
- }
+
+
+    public static List<String> mainNotificationTitleSpanish = Arrays.asList(
+            "¡No me olvides!",
+            "¡Es hora de calcular!",
+            "¡Tiempo de cálculo!",
+            "¡RechenMax está esperando!"
+    );
+
+    public static List<String> mainNotificationContentSpanish = Arrays.asList(
+            "¡Hola! Hace un tiempo que no has hecho cálculos. Quizás sea hora de hacerlo de nuevo.",
+            "Tu RechenMax se aburre. ¡Es hora de hacer algunos cálculos complicados!",
+            "¡Calcula la respuesta a la vida, el universo y todo!",
+            "¡RechenMax está emocionado de verte!"
+    );
+
+    public static List<String> notificationHintsListSpanish = Arrays.asList(
+            "RechenMax fue creado solo por una persona.",
+            "Puedes personalizar casi todo en la configuración.",
+            "Recibe instrucciones haciendo clic en el signo de interrogación en la configuración.",
+            "El nombre de la aplicación proviene del tío del inventor.",
+            "La versión de Android de RechenMax cobró vida alrededor del 01.10.23.",
+            "También hay una versión de Windows de RechenMax.",
+            "Puedes acceder a más funciones presionando el botón en la esquina superior izquierda.",
+            "El modo de cálculo simplificado en la configuración puede hacer muchas cosas más fáciles.",
+            "RechenMax cumple años el 01.10.2023.",
+            "Originalmente, la calculadora estaba destinada solo para Windows.",
+            "La calculadora fue desarrollada en Alemania.",
+            "42 es la respuesta a todo el universo.",
+            "En RechenMax, además de las operaciones básicas, hay otras funciones.",
+            "La calculadora fue creada utilizando Java.",
+            "RechenMax se actualiza regularmente.",
+            "El trabajo en la calculadora está en curso constante.",
+            "Espero que te guste RechenMax.",
+            "RechenMax fue el primer proyecto importante del desarrollador.",
+            "Tus cálculos están guardados de forma segura.",
+            "Puedes personalizar el color de la calculadora en la configuración.",
+            "¡RechenMax puede hacer mucho más que solo matemáticas básicas!",
+            "En la sección de ayuda en la configuración, encontrarás consejos útiles.",
+            "Puedes encontrar RechenMax y otros proyectos en GitHub.",
+            "Visita: 'https://sites.google.com/view/ml-programs' para obtener más información.",
+            "1 + 1 es igual a 2.",
+            "Visita: 'https://github.com/gamingPanther3' para obtener más información."
+    );
+}
