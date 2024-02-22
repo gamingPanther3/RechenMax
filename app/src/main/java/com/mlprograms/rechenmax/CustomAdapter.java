bd9deb938d2bfbb21c031da30d7973b87137b2c0
gamingPanther3 <max.lemberg2402@gmail.com>
2024-02-23 00:46:04 +0100

    added more functions (setTextColor, setBackgroundColor...)

--- app/src/main/java/com/mlprograms/rechenmax/CustomAdapter.java
+++ app/src/main/java/com/mlprograms/rechenmax/CustomAdapter.java
@@ -1,6 +1,7 @@
 package com.mlprograms.rechenmax;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,9 +16,19 @@ import java.util.ArrayList;
 
 public class CustomAdapter extends ArrayAdapter<CustomItems> {
 
+    private int defaultTextColor = Color.BLACK;
+    private int dropdownBackgroundColor = Color.WHITE;
 
     public CustomAdapter(@NonNull Context context, ArrayList<CustomItems> customList) {
         super(context, 0, customList);
+    }
+
+    public void setTextColor(int color) {
+        defaultTextColor = color;
+    }
+
+    public void setBackgroundColor(int color) {
+        dropdownBackgroundColor = color;
     }
 
     @NonNull
@@ -26,9 +37,21 @@ public class CustomAdapter extends ArrayAdapter<CustomItems> {
         return customView(position, convertView, parent);
     }
 
-    @Override
     public View getDropDownView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        return customView(position, convertView, parent);
+        if(convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.convert_spinner_layout, parent, false);
+        }
+        CustomItems items = getItem(position);
+        ImageView spinnerImage = convertView.findViewById(R.id.ivCustomSpinner);
+        TextView spinnerName = convertView.findViewById(R.id.tvCustomSpinner);
+        if(items != null) {
+            spinnerImage.setImageResource(items.getSpinnerImage());
+            spinnerName.setText(items.getSpinnerText());
+
+            spinnerName.setTextColor(defaultTextColor);
+            convertView.setBackgroundColor(dropdownBackgroundColor);
+        }
+        return convertView;
     }
 
     public View customView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
@@ -41,6 +64,7 @@ public class CustomAdapter extends ArrayAdapter<CustomItems> {
         if(items != null) {
             spinnerImage.setImageResource(items.getSpinnerImage());
             spinnerName.setText(items.getSpinnerText());
+            spinnerName.setTextColor(defaultTextColor);
         }
         return convertView;
     }
--- app/src/main/java/com/mlprograms/rechenmax/CustomItems.java
+++ app/src/main/java/com/mlprograms/rechenmax/CustomItems.java
@@ -2,8 +2,10 @@ package com.mlprograms.rechenmax;
 
 public class CustomItems {
 
-    private final String spinnerText;
-    private final int spinnerImage;
+    private String spinnerText;
+    private int spinnerImage;
+    private int textColor;
+    private int backgroundColor;
 
     public CustomItems(String spinnerText, int spinnerImage) {
         this.spinnerText = spinnerText;
@@ -14,7 +16,31 @@ public class CustomItems {
         return spinnerText;
     }
 
+    public void setSpinnerText(String spinnerText) {
+        this.spinnerText = spinnerText;
+    }
+
     public int getSpinnerImage() {
         return spinnerImage;
+    }
+
+    public void setSpinnerImage(int spinnerImage) {
+        this.spinnerImage = spinnerImage;
+    }
+
+    public int getTextColor() {
+        return textColor;
+    }
+
+    public void setTextColor(int textColor) {
+        this.textColor = textColor;
+    }
+
+    public int getBackgroundColor() {
+        return backgroundColor;
+    }
+
+    public void setBackgroundColor(int color) {
+        this.backgroundColor = color;
     }
 }
