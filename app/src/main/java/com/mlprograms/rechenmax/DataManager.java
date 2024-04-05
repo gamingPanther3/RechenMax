a3a392d548f9f1dbbe665e598307f35a9aa81aaf
gamingPanther3 <max.lemberg2402@gmail.com>
2024-04-05 21:36:47 +0200

    fixed imports

--- app/src/main/java/com/mlprograms/rechenmax/DataManager.java
+++ app/src/main/java/com/mlprograms/rechenmax/DataManager.java
@@ -28,13 +28,9 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 /**
  * DataManager
