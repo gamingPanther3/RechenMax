7335656d986df74f7d19995ebaea4fa6af22eaae
gamingPanther3 <max.lemberg2402@gmail.com>
2024-05-10 00:30:55 +0200

    fixed bugs

--- app/src/main/java/com/mlprograms/rechenmax/DataManager.java
+++ app/src/main/java/com/mlprograms/rechenmax/DataManager.java
@@ -393,7 +393,7 @@ public class DataManager {
             initializeSetting("shiftRow", "1", applicationContext);
             initializeSetting("logX", "false", applicationContext);
             initializeSetting("calculationMode", "Vereinfacht", applicationContext);
-            initializeSetting("currentVersion", "1.6.5", applicationContext);
+            initializeSetting("currentVersion", "1.6.6", applicationContext);
             initializeSetting("old_version", "0", applicationContext);
             initializeSetting("returnToCalculator", "false", applicationContext);
             initializeSetting("allowNotification", "false", applicationContext);
